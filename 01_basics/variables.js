bc12f3956166729de0c73f65e0aa1524d6b487c4
Piyush Goyal <155963774+Piyush-goyal13@users.noreply.github.com>
2026-02-23 16:00:17 +0000

    1st days

---
+++ 01_basics/variables.js
@@ -0,0 +1,14 @@
+const accountId=123;
+
+var accountEmail="piyush@gmail.com";
+let accountPassword=12344;
+accountHolder="Piyush";
+
+console.table([accountId,accountEmail,accountPassword,accountHolder]);
+
+
+// we use const when we dont wanna change the value in furture.
+// we will use let nowdays to declare variables instead of var because of some isssues.
+// var is not blocked scoped hence will change the unwanted variables.
+
+
